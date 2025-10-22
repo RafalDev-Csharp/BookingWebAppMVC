b5e8e003e35d367d4b52b3f71ef3432a0c9585c9
RafalDev-Csharp <64135312+RafalDev-Csharp@users.noreply.github.com>
2025-10-22 22:22:18 +0200

    Added 'House' entity to Booking.Domain proj

---
+++ Booking.Domain/Entities/House.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booking.Domain.Entities
+{
+    public class House
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public string? Description { get; set; }
+        public double Price { get; set; }
+        public int SqMeters { get; set; } //square meters
+        public int Occupancy { get; set; }
+        public string? ImageUrl { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+    }
+}
