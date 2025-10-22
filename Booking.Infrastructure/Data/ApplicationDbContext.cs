cf31e943a33319d40470c6243246a4ef23b15573
RafalDev-Csharp <64135312+RafalDev-Csharp@users.noreply.github.com>
2025-10-23 00:06:11 +0200

    Houses seed 3 example into the table - override OnModelCreating

--- Booking.Infrastructure/Data/ApplicationDbContext.cs
+++ Booking.Infrastructure/Data/ApplicationDbContext.cs
@@ -16,5 +16,42 @@ namespace Booking.Infrastructure.Data
         }
 
         public DbSet<House> Houses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<House>().HasData(
+                new House
+                { 
+                    Id = 1,
+                    Name = "Big House",
+                    Description = "Description about Big House...",
+                    ImageUrl = "https://placehold.co/600x400",
+                    Occupancy = 4,
+                    Price = 350,
+                    SqMeters = 600
+                },
+                new House
+                {
+                    Id = 2,
+                    Name = "Uber House",
+                    Description = "Description about Uber House...",
+                    ImageUrl = "https://placehold.co/600x401",
+                    Occupancy = 3,
+                    Price = 250,
+                    SqMeters = 500
+                },
+                new House
+                {
+                    Id = 3,
+                    Name = "Boss House",
+                    Description = "Description about Boss House...",
+                    ImageUrl = "https://placehold.co/600x402",
+                    Occupancy = 6,
+                    Price = 150,
+                    SqMeters = 750
+                });
+        }
     }
 }
---
+++ Booking.Infrastructure/Migrations/20251022220226_Seed3ExampleHousesIntoTable.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Booking.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Seed3ExampleHousesIntoTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Houses",
+                columns: new[] { "Id", "CreatedDate", "Description", "ImageUrl", "Name", "Occupancy", "Price", "SqMeters", "UpdatedDate" },
+                values: new object[,]
+                {
+                    { 1, null, "Description about Big House...", "https://placehold.co/600x400", "Big House", 4, 350.0, 600, null },
+                    { 2, null, "Description about Uber House...", "https://placehold.co/600x401", "Uber House", 3, 250.0, 500, null },
+                    { 3, null, "Description about Boss House...", "https://placehold.co/600x402", "Boss House", 6, 150.0, 750, null }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Houses",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Houses",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Houses",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
