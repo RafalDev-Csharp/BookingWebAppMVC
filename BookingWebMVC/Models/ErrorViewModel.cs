9470e66c87bc0a9094af2292fc444ee63bedf2e8
RafalDev-Csharp <64135312+RafalDev-Csharp@users.noreply.github.com>
2025-10-22 21:40:51 +0200

    Add project files.

---
+++ BookingWebMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BookingWebMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
