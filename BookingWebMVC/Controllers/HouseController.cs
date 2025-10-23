94c4e525d708c6636e71ab3bb38827c4157adf8b
RafalDev-Csharp <64135312+RafalDev-Csharp@users.noreply.github.com>
2025-10-23 11:38:24 +0200

    Added Houses/Index view - List<Houses> Added css and bootstrap 5.3.8 Added dark theme

---
+++ BookingWebMVC/Controllers/HouseController.cs
@@ -0,0 +1,21 @@
+﻿using Booking.Domain.Entities;
+using Booking.Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookingWebMVC.Controllers
+{
+    public class HouseController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public HouseController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public IActionResult Index()
+        {
+            var output = _dbContext.Houses.ToList<House>();
+            return View(output);
+        }
+    }
+}
