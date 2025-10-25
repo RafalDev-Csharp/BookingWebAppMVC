3322544e44c9617c0063803d3d94b2db80b3a61f
RafalDev-Csharp <64135312+RafalDev-Csharp@users.noreply.github.com>
2025-10-25 14:56:49 +0200

    Added Update and Delete actions and views

--- BookingWebMVC/Controllers/HouseNumberController.cs
+++ BookingWebMVC/Controllers/HouseNumberController.cs
@@ -1,5 +1,6 @@
 ﻿using Booking.Domain.Entities;
 using Booking.Infrastructure.Data;
+using BookingWebMVC.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -16,123 +17,169 @@ namespace BookingWebMVC.Controllers
         }
         public async Task<IActionResult> Index()
         {
-            var output = await _dbContext.HouseNumbers.ToListAsync();
+            var output = await _dbContext.HouseNumbers.Include(x => x.House).ToListAsync();
             return View(output);
         }
 
         public async Task<IActionResult> Create()
         {
-            IEnumerable<SelectListItem> list = _dbContext.Houses.ToList().Select(x => new SelectListItem
+            HouseNumberVM houseNumberVM = new()
             {
-                Text = x.Name,
-                Value = x.Id.ToString()
-            });
-            // ViewDate used to provide data from controller to the view
-            ViewData["HousesSelectListItems"] = list;
-            return View();
+                HousesList = _dbContext.Houses.ToList().Select(x => new SelectListItem
+                {
+                    Text = x.Name,
+                    Value = x.Id.ToString()
+                }),
+                //HouseNumber = await _dbContext.HouseNumbers
+            };
+            // ViewData used to provide data from controller to the view
+            //is a type of Dictionary
+            //ViewBag is a dynamic type property
+            //ViewData["HousesSelectListItems"] = list;
+            //ViewBag.MyHousesList = list;
+            return View(houseNumberVM);
         }
 
         [HttpPost]
-        public async Task<IActionResult> Create([FromForm] HouseNumber houseNumber)
+        public async Task<IActionResult> Create(HouseNumberVM houseNumberVM)
         {
-
-            //this is a navigation property and it won't be validate 
-            //ModelState.Remove("House");
-            //or add to property House: [ValidateNever]
-
-            if (houseNumber == null)
+            bool isHouseNumberExists = _dbContext.HouseNumbers.Any(x => x.House_Number == houseNumberVM.HouseNumber.House_Number);
+            if (isHouseNumberExists)
+            {
+                TempData["error"] = "The House Number already exists, enter the another number";
+                houseNumberVM = new()
+                {
+                    HousesList = _dbContext.Houses.ToList().Select(x => new SelectListItem
+                    {
+                        Text = x.Name,
+                        Value = x.Id.ToString()
+                    }),
+                };
+                return View(houseNumberVM);
+            }
+            if (houseNumberVM == null)
             {
                 ModelState.AddModelError(@"model", @"'HouseNumber' object cannot be null");
-                return View();
+                TempData["error"] = "The House Number cannot be null";
+                return View(houseNumberVM);
             }
             if (ModelState.IsValid == false)
             {
                 ModelState.AddModelError(@"model", @"'HouseNumber' object is not valid, fill the form properly");
-                return View(houseNumber);
+                TempData["error"] = "The House Number is not valid";
+                return View(houseNumberVM);
             }
             try
             {
-                await _dbContext.HouseNumbers.AddAsync(houseNumber);
+                await _dbContext.HouseNumbers.AddAsync(houseNumberVM.HouseNumber);
                 await _dbContext.SaveChangesAsync();
                 TempData["success"] = "The house has been created successfully.";
             }
             catch (Exception ex)
             {
+                TempData["error"] = "Something went wrong while saving House Number to database";
                 throw new Exception("cannot create new HouseNumber object.. Error occurred.", ex);
             }
             return RedirectToAction(nameof(Index), "HouseNumber");
-            //return RedirectToAction(nameof(Index), new { house });
         }
 
 
-        public async Task<IActionResult> Update(int houseId)
+
+        public async Task<IActionResult> Update(int houseNumberId)
         {
-            //var houseToUpdate = _dbContext.Houses.Find(houseId);
-            var houseToUpdate = await _dbContext.Houses.FirstOrDefaultAsync(h => h.Id == houseId);
-            if (houseToUpdate is null)
+            HouseNumberVM houseNumberToUpdateVM = new();
+            houseNumberToUpdateVM.HousesList = _dbContext.Houses.ToList()
+                .Select(x => new SelectListItem
+                {
+                    Text = x.Name,
+                    Value = x.Id.ToString()
+                });
+            houseNumberToUpdateVM.HouseNumber = await _dbContext.HouseNumbers.Include(x => x.House).FirstOrDefaultAsync(h => h.House_Number == houseNumberId);
+            if (houseNumberToUpdateVM.HouseNumber is null)
             {
+                TempData["error"] = $"Something went wrong while reading the data of House Number with Id = {houseNumberId}";
                 return RedirectToAction("Error", "Home");
             }
-
-            return View(houseToUpdate);
+            return View(houseNumberToUpdateVM);
         }
 
         [HttpPost]
-        public async Task<IActionResult> Update([FromForm] House house)
+        public async Task<IActionResult> Update(HouseNumberVM? houseNumberToUpdateVM)
         {
-            var housesList = await _dbContext.Houses.Where(x => string.Equals(x.Name.ToLower(), house.Name.ToLower())).FirstOrDefaultAsync();
-            if (housesList != null)
+            //var houseNumberExists = await _dbContext.HouseNumbers.Where(x => x.House_Number == houseNumberToUpdateVM.HouseNumber.House_Number).ToListAsync();
+            houseNumberToUpdateVM.HousesList = _dbContext.Houses.ToList()
+                .Select(x => new SelectListItem
             {
-                ModelState.AddModelError("Name", "Model with this name already exists.");
-                return View(house);
-            }
-            if (house == null)
+                Text = x.Name,
+                Value = x.Id.ToString()
+            });
+
+            //if (houseNumberExists.Count > 1)
+            //{
+            //    ModelState.AddModelError("Name", "Model with this number/Id already exists.");
+            //    TempData["error"] = "House Number with Id = { } already exists";
+            //    return View(houseNumberToUpdateVM);
+            //}
+            if (houseNumberToUpdateVM.HouseNumber == null)
             {
                 ModelState.AddModelError("Model", "This house not exists. Probably it was removed.");
+                TempData["error"] = "This house not exists. Probably it was removed.";
                 return RedirectToAction("Error", "Home");
             }
             try
             {
-                _dbContext.Houses.Update(house);
+                _dbContext.HouseNumbers.Update(houseNumberToUpdateVM.HouseNumber);
                 await _dbContext.SaveChangesAsync();
-                TempData["success"] = "The house has been updated successfully.";
-                return RedirectToAction("Index", "House");
+                TempData["success"] = "The House Number has been updated successfully.";
+                return RedirectToAction("Index", "HouseNumber");
             }
             catch (Exception ex)
             {
-                throw new Exception("cannot create new House object.. Error occurred.", ex);
+                throw new Exception("cannot create new House Number object.. some Error occurred.", ex);
             }
 
         }
 
 
-        public async Task<IActionResult> Delete(int houseId)
+        public async Task<IActionResult> Delete(int houseNumberId)
         {
-            //var houseToUpdate = _dbContext.Houses.Find(houseId);
-            var houseToDelete = await _dbContext.Houses.FirstOrDefaultAsync(h => h.Id == houseId);
-            if (houseToDelete is null)
+            HouseNumberVM houseNumberToDeleteVM = new();
+            houseNumberToDeleteVM.HousesList = _dbContext.Houses.ToList()
+                .Select(x => new SelectListItem
+                {
+                    Text = x.Name,
+                    Value = x.Id.ToString()
+                });
+            houseNumberToDeleteVM.HouseNumber = await _dbContext.HouseNumbers.Include(x => x.House).FirstOrDefaultAsync(h => h.House_Number == houseNumberId);
+            if (houseNumberToDeleteVM.HouseNumber is null)
             {
+                TempData["error"] = $"Something went wrong while reading the data of House Number with Id = {houseNumberId}";
                 return RedirectToAction("Error", "Home");
             }
-
-            return View(houseToDelete);
+            return View(houseNumberToDeleteVM);
         }
 
         [HttpPost]
-        public async Task<IActionResult> Delete(House house)
+        public async Task<IActionResult> Delete(HouseNumberVM? houseNumberToDeleteVM)
         {
-            var houseToDelete = await _dbContext.Houses.FirstOrDefaultAsync(h => h.Id == house.Id);
-            if (houseToDelete is null)
+            houseNumberToDeleteVM.HousesList = _dbContext.Houses.ToList()
+                .Select(x => new SelectListItem
+                {
+                    Text = x.Name,
+                    Value = x.Id.ToString()
+                });
+            houseNumberToDeleteVM.HouseNumber = await _dbContext.HouseNumbers.FirstOrDefaultAsync(x => x.House_Number == houseNumberToDeleteVM.HouseNumber.House_Number);
+            if (houseNumberToDeleteVM.HouseNumber is null)
             {
-                TempData["error"] = "The house could not be deleted";
+                TempData["error"] = "The house number could not be deleted";
                 return RedirectToAction("Error", "Home");
             }
 
-            _dbContext.Houses.Remove(houseToDelete);
+            _dbContext.HouseNumbers.Remove(houseNumberToDeleteVM.HouseNumber);
             await _dbContext.SaveChangesAsync();
             TempData["success"] = "The house has been removed successfully.";
 
-            return RedirectToAction("Index", "House");
+            return RedirectToAction("Index", "HouseNumber");
         }
     }
 }
---
+++ BookingWebMVC/ViewModels/HouseNumberVM.cs
@@ -0,0 +1,14 @@
+﻿using Booking.Domain.Entities;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace BookingWebMVC.ViewModels
+{
+    public class HouseNumberVM
+    {
+        public HouseNumber? HouseNumber { get; set; }
+
+        [ValidateNever]
+        public IEnumerable<SelectListItem>? HousesList { get; set; }
+    }
+}
