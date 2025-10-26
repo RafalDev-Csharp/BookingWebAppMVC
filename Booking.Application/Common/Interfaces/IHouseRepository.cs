cd4321471a21924731e6ec88141faa8e4b19b455
RafalDev-Csharp <64135312+RafalDev-Csharp@users.noreply.github.com>
2025-10-26 16:01:17 +0100

    Added IHouseRepository and HouseRepository plus service in Program.cs webApp

---
+++ Booking.Application/Common/Interfaces/IHouseRepository.cs
@@ -0,0 +1,20 @@
+﻿using Booking.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booking.Application.Common.Interfaces
+{
+    public interface IHouseRepository
+    {
+        IEnumerable<House> GetAll(Expression<Func<House, bool>>? filter = null, string? includeProperties = null);
+        IEnumerable<House> Get(Expression<Func<House, bool>> filter, string? includeProperties = null);
+        void Add(House house);
+        void Update(House house);
+        void Remove(House house);
+        void Save();
+    }
+}
---
+++ Booking.Infrastructure/Repository/HouseRepository.cs
@@ -0,0 +1,52 @@
+﻿using Booking.Application.Common.Interfaces;
+using Booking.Domain.Entities;
+using Booking.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booking.Infrastructure.Repository
+{
+    public class HouseRepository : IHouseRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public HouseRepository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public void Add(House house)
+        {
+            _dbContext.Add<House>(house);
+        }
+
+        public IEnumerable<House> Get(Expression<Func<House, bool>> filter, string? includeProperties = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<House> GetAll(Expression<Func<House, bool>>? filter = null, string? includeProperties = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(House house)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(House house)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
